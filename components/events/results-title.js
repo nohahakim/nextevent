694ca4f3a60038780985446238d2019f84c51e4c
nohahakim <raafatabass@hotmail.com>
2025-07-04 21:13:42 +0300

    filtered-events

---
+++ components/events/results-title.js
@@ -0,0 +1,16 @@
+import classes from "./results-title.module.css";
+
+function ResultsTitle({ date }) {
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+  });
+
+  return (
+    <section className={classes.title}>
+      <h1>Events in {humanReadableDate}</h1>
+    </section>
+  );
+}
+
+export default ResultsTitle;
