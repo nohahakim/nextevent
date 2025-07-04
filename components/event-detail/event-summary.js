ee331e7b615c06c0b0bd47b77ea56a63a091dcec
nohahakim <raafatabass@hotmail.com>
2025-07-04 13:49:08 +0300

    event-details

--- components/event-detail/event-summary.js
+++ components/event-detail/event-summary.js
@@ -1,5 +1,13 @@
-import React from "react";
+import classes from './event-summary.module.css';
 
-export default function EventSummary() {
-  return <div>EventSummary</div>;
+function EventSummary(props) {
+  const { title } = props;
+
+  return (
+    <section className={classes.summary}>
+      <h1>{title}</h1>
+    </section>
+  );
 }
+
+export default EventSummary;
